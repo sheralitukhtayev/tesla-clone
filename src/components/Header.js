9b94094e86afaf610f5ec5a139f45bd253d86ce8
SheraliTukhtayev <sheralitukhtayev@gmail.com>
2022-06-21 11:20:12 +0500

    change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,73 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Header() {
   return (
-    <div>Header</div>
+    <Container>
+      <a href="#">
+        <img src="images/logo.svg" alt=""/>
+      </a>
+
+      <Menu>
+        <a href="#">Model S</a>
+        <a href="#">Model 3</a>
+        <a href="#">Model X</a>
+        <a href="#">Model Y</a>
+      </Menu>
+      <RightMenu>
+        <a  href="#">Shop</a>
+        <a  href="#">Tesla Account</a>
+        <CustomMenu>
+          <img src="images/menu.png" />
+        </CustomMenu>
+      </RightMenu>
+    </Container>
   )
 }
 
 export default Header
+
+const Container = styled.div `
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`
+
+const Menu = styled.div `
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+
+`
+
+const RightMenu = styled.div `
+  display: flex;
+  align-items: center;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 15px;
+  }
+
+`
+
+const CustomMenu = styled.div`
+  img {
+    width: 20px;
+  }
+
+`
