a3cf88f46ab48a903becd9f8f4dc35869b63e931
SheraliTukhtayev <sheralitukhtayev@gmail.com>
2022-06-23 21:48:47 +0500

    add cars name to menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,6 +31,10 @@ function Header() {
             <img src="images/x.png" onClick={()=>setBurgerStatus(false)} />
           </CustomClose>
         </CloseWrapper>
+        <li><a href="#">Model S</a></li>
+        <li><a href="#">Model 3</a></li>
+        <li><a href="#">Model X</a></li>
+        <li><a href="#">Model Y</a></li>
         <li><a href="#">Existing Inventory</a></li>
         <li><a href="#">Used Inventory</a></li>
         <li><a href="#">Trade-in</a></li>
